cc3b98165b799c203db180f6f213710407aa89a9
Justin Buchanan <jbuch@buchmail.com>
2017-06-12 08:29:26 -0500

    Sample code

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Dapper;
+using AppServiceDemo1.Models;
+
+namespace AppServiceDemo1.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            string connectionString = "Server=tcp:csg-sql-bargeops.database.windows.net,1433;Initial Catalog=Lunch1;Persist Security Info=False;User ID=lunch1;Password=RCMqFuNltvAC7EHF8ysp57;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+            using (var db = new System.Data.SqlClient.SqlConnection(connectionString))
+            {
+                db.Open();
+
+                var customers = db.Query<CustomerModel>(@"
+                    SELECT * 
+                    FROM SalesLT.Customer 
+                    ORDER BY LastName, FirstName"
+                );
+
+                return View(customers);
+            }
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        [AllowAnonymous]
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/CustomerModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppServiceDemo1.Models
+{
+    public class CustomerModel
+    {
+        public string Title { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string CompanyName { get; set; }
+
+        public string EmailAddress { get; set; }
+    }
+}
